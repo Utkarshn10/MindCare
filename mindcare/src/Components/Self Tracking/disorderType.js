bce69e210af2ad573194fa62dcb3498f156b899a
Utkarsh10 <utkarshnagar2000@gmail.com>
2021-05-28 18:43:26 +0530

    Assessment added

---
+++ mindcare/src/Components/Self Tracking/disorderType.js
@@ -0,0 +1,26 @@
+import React from "react";
+import image from "./image/logo6.png";
+import "./tracking.css";
+import ButtonBases from "./option";
+// import fullLogo from '../../../assets/fullLogo.jpeg'
+
+function choose() {
+  return (
+    <div>
+      <h2 className="topbar">Soothe</h2>
+      <div className="belowtopbar">
+        <img className="image-handle"></img>
+        <h1 class="headingheadddd">Self Tracking and Assessment Portal</h1>
+      </div>
+
+      <div className="center OptionDivision">
+        <h3 class="feelingText">I'm feeling ......</h3>
+        <div class="imageButtons">
+          <ButtonBases />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default choose;
